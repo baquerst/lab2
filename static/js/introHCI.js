f2042996ea430cb2439015e7c5a917fad2aa0245
Bryan Lee <blee379@gmail.com>
2020-01-16 10:23:28 -0800

    fixed js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,6 +8,7 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+            $("a.thumbnail").click(projectClick);
 }
 
 function projectClick(e) { 
